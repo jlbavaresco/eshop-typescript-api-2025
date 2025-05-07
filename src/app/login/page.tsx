0d8c4afe0f2fe41a88d85af737ac2d3fb87923b9
Jorge Bavaresco <jlbavaresco@gmail.com>
2025-05-07 15:14:50 -0300

    ajuste na tela de login para exibir carregametno

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -4,23 +4,26 @@ import Button from 'react-bootstrap/Button';
 import Form from 'react-bootstrap/Form';
 import { signIn } from 'next-auth/react';
 import { useRouter } from 'next/navigation';
+import Carregando from '@/componentes/Carregando';
 
 export default function Login() {
     const [email, setEmail] = useState('');
     const [senha, setSenha] = useState('');
     const [erroLogin, setErroLogin] = useState(false);
+    const [carregando, setCarregando] = useState(false);
 
     const router = useRouter();
 
     const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
         e.preventDefault();
         setErroLogin(false);
-
+        setCarregando(true);
         const res = await signIn('credentials', {
             email,
             senha,
             redirect: false,
         });
+        setCarregando(false);
 
         if (!res || !res.ok) {
             setErroLogin(true);
@@ -40,35 +43,37 @@ export default function Login() {
                         Falha ao efetuar o login. Usuário ou senha inválidos.
                     </h4>
                 )}
-                <div className="col-12 col-md-6">
-                    <Form onSubmit={handleSubmit}>
-                        <Form.Group className="mb-3" controlId="txtEmail">
-                            <Form.Label>Email</Form.Label>
-                            <Form.Control
-                                type="email"
-                                placeholder="Informe o email"
-                                value={email}
-                                onChange={(e) => setEmail(e.target.value)}
-                                required
-                            />
-                        </Form.Group>
-                        <Form.Group className="mb-3" controlId="txtSenha">
-                            <Form.Label>Senha</Form.Label>
-                            <Form.Control
-                                type="password"
-                                placeholder="Senha"
-                                value={senha}
-                                onChange={(e) => setSenha(e.target.value)}
-                                required
-                            />
-                        </Form.Group>
-                        <div className="form-group text-center mt-3">
-                            <Button variant="primary" type="submit">
-                                Efetuar Login
-                            </Button>
-                        </div>
-                    </Form>
-                </div>
+                <Carregando carregando={carregando}>
+                    <div className="col-12 col-md-6">
+                        <Form onSubmit={handleSubmit}>
+                            <Form.Group className="mb-3" controlId="txtEmail">
+                                <Form.Label>Email</Form.Label>
+                                <Form.Control
+                                    type="email"
+                                    placeholder="Informe o email"
+                                    value={email}
+                                    onChange={(e) => setEmail(e.target.value)}
+                                    required
+                                />
+                            </Form.Group>
+                            <Form.Group className="mb-3" controlId="txtSenha">
+                                <Form.Label>Senha</Form.Label>
+                                <Form.Control
+                                    type="password"
+                                    placeholder="Senha"
+                                    value={senha}
+                                    onChange={(e) => setSenha(e.target.value)}
+                                    required
+                                />
+                            </Form.Group>
+                            <div className="form-group text-center mt-3">
+                                <Button variant="primary" type="submit">
+                                    Efetuar Login
+                                </Button>
+                            </div>
+                        </Form>
+                    </div>
+                </Carregando>
             </div>
         </div>
     );
